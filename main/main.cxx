d00f94c1c2e2568a86d8e9e24521a4d70ff9b96e
TalRasha <524948250@qq.com>
2025-08-21 19:46:12 +0800

    use a MACRO to enable unit test

--- main/main.cxx
+++ main/main.cxx
@@ -1,5 +1,11 @@
+#include <iostream>
+
 int main(int argc, char ** argv)
 {
     (void) argc, (void) argv;
+    #ifdef ENABLE_UNIT_TEST
+    std::cout << "unit test is enabled" << std::endl;
+
+    #endif
     return 0;
 }
